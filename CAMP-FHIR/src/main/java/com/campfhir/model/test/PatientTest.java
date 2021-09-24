cc67c019d99728850df0c57ca10021a6f255a01e
James Champion <champioj@email.unc.edu>
2021-09-24 11:30:31 -0400

    Adding initial unit test for Patient

---
+++ CAMP-FHIR/src/main/java/com/campfhir/model/test/PatientTest.java
@@ -0,0 +1,32 @@
+package main.java.com.campfhir.model.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.hibernate.ScrollableResults;
+import org.junit.jupiter.api.Test;
+
+import main.java.com.campfhir.dao.PatientDao;
+import main.java.com.campfhir.model.Patient;
+
+
+class PatientTest 
+{
+	
+	private static PatientDao patientDao;
+	
+	@Test
+	void test() 
+	{
+		ScrollableResults patients = patientDao.findAll();
+		
+		Patient p = new Patient();
+		
+	    while ( patients.next() ) 
+	    {	
+	    	p = (Patient) patients.get(0);
+	    }
+		
+		fail("Not yet implemented");
+	}
+
+}
