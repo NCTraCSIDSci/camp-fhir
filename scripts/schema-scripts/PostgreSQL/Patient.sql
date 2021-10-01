489fcaf360c4326d386ccc44ad1bfda3ea4ad7ca
asiyahmad <90564381+asiyahmad@users.noreply.github.com>
2021-10-01 11:05:38 -0400

    Added contact address field to the script and hibernate files

--- scripts/schema-scripts/PostgreSQL/Patient.sql
+++ scripts/schema-scripts/PostgreSQL/Patient.sql
@@ -21,6 +21,7 @@ CREATE TABLE IF NOT EXISTS public."Patient"
     "PNT_CONTACT_RELATIONSHIP" text COLLATA pg_catalog."default",
     "PNT_CONTACT_NAME" text COLLATA pg_catalog."default",
     "PNT_CONTACT_TELECOM" text COLLATA pg_catalog."default",
+    "PNT_CONTACT_ADDRESS" text COLLATA pg_catalog."default",
     "PNT_RACE" text COLLATE pg_catalog."default",
     "PNT_RACE_DISP" text COLLATE pg_catalog."default",
     "PNT_ETHNICITY" text COLLATE pg_catalog."default",
