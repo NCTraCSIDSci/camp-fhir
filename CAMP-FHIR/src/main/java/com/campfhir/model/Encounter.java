addac7109882b7304f9fb83247e5c4854b3d085d
asiyahmad <90564381+asiyahmad@users.noreply.github.com>
2021-10-29 12:33:10 -0400

    adding this key to the encounter model class

--- CAMP-FHIR/src/main/java/com/campfhir/model/Encounter.java
+++ CAMP-FHIR/src/main/java/com/campfhir/model/Encounter.java
@@ -36,17 +36,12 @@ public class Encounter
 	
 	public String getENC_IDENTIFIER() 
 	{
-		return ENC_IDENTIFIER;
+		return this.ENC_IDENTIFIER;
 	}
 
 	public void setENC_IDENTIFIER(String eNC_IDENTIFIER) 
 	{
-		ENC_IDENTIFIER = eNC_IDENTIFIER;
-	}
-	
-	public String getENC_IDENTIFIER() 
-	{
-		return ENC_IDENTIFIER;
+		this.ENC_IDENTIFIER = eNC_IDENTIFIER;
 	}
 	
 	public String getENC_SERVICE_TYPE_CODEABLECONCEPT_CODING() {
@@ -68,167 +63,167 @@ public class Encounter
 
 	public void setENC_SUBJECT_REFERENCE(String eNC_SUBJECT_REFERENCE) 
 	{
-		ENC_SUBJECT_REFERENCE = eNC_SUBJECT_REFERENCE;
+		this.ENC_SUBJECT_REFERENCE = eNC_SUBJECT_REFERENCE;
 	}
 
 	public String getENC_CLASS_SYST() 
 	{
-		return ENC_CLASS_SYST;
+		return this.ENC_CLASS_SYST;
 	}
 
 	public void setENC_CLASS_SYST(String eNC_CLASS_SYST) 
 	{
-		ENC_CLASS_SYST = eNC_CLASS_SYST;
+		this.ENC_CLASS_SYST = eNC_CLASS_SYST;
 	}
 
 	public String getENC_CLASS_CODE() 
 	{
-		return ENC_CLASS_CODE;
+		return this.ENC_CLASS_CODE;
 	}
 
 	public void setENC_CLASS_CODE(String eNC_CLASS_CODE) 
 	{
-		ENC_CLASS_CODE = eNC_CLASS_CODE;
+		this.ENC_CLASS_CODE = eNC_CLASS_CODE;
 	}
 
 	public String getENC_CLASS_DISPLAY() 
 	{
-		return ENC_CLASS_DISPLAY;
+		return this.ENC_CLASS_DISPLAY;
 	}
 
 	public void setENC_CLASS_DISPLAY(String eNC_CLASS_DISPLAY) 
 	{
-		ENC_CLASS_DISPLAY = eNC_CLASS_DISPLAY;
+		this.ENC_CLASS_DISPLAY = eNC_CLASS_DISPLAY;
 	}
 
 	public String getENC_PERIOD_START() 
 	{
-		return ENC_PERIOD_START;
+		return this.ENC_PERIOD_START;
 	}
 
 	public void setENC_PERIOD_START(String eNC_PERIOD_START) 
 	{
-		ENC_PERIOD_START = eNC_PERIOD_START;
+		this.ENC_PERIOD_START = eNC_PERIOD_START;
 	}
 
 	public String getENC_PERIOD_END() 
 	{
-		return ENC_PERIOD_END;
+		return this.ENC_PERIOD_END;
 	}
 
 	public void setENC_PERIOD_END(String eNC_PERIOD_END) 
 	{
-		ENC_PERIOD_END = eNC_PERIOD_END;
+		this.ENC_PERIOD_END = eNC_PERIOD_END;
 	}
 
 	public String getENC_HOSP_DISDISP_CODING_CODE() 
 	{
-		return ENC_HOSP_DISDISP_CODING_CODE;
+		return this.ENC_HOSP_DISDISP_CODING_CODE;
 	}
 
 	public void setENC_HOSP_DISDISP_CODING_CODE(String eNC_HOSP_DISDISP_CODING_CODE) 
 	{
-		ENC_HOSP_DISDISP_CODING_CODE = eNC_HOSP_DISDISP_CODING_CODE;
+		this.ENC_HOSP_DISDISP_CODING_CODE = eNC_HOSP_DISDISP_CODING_CODE;
 	}
 
 	public String getENC_HOSP_DISDISP_TEXT() 
 	{
-		return ENC_HOSP_DISDISP_TEXT;
+		return this.ENC_HOSP_DISDISP_TEXT;
 	}
 
 	public void setENC_HOSP_DISDISP_TEXT(String eNC_HOSP_DISDISP_TEXT) 
 	{
-		ENC_HOSP_DISDISP_TEXT = eNC_HOSP_DISDISP_TEXT;
+		this.ENC_HOSP_DISDISP_TEXT = eNC_HOSP_DISDISP_TEXT;
 	}
 	
 	public String getENC_PARTICIPANT_INDIV_REF() 
 	{
-		return ENC_PARTICIPANT_INDIV_REF;
+		return this.ENC_PARTICIPANT_INDIV_REF;
 	}
 
 	public void setENC_PARTICIPANT_INDIV_REF(String eNC_PARTICIPANT_INDIV_REF) 
 	{
-		ENC_PARTICIPANT_INDIV_REF = eNC_PARTICIPANT_INDIV_REF;
+		this.ENC_PARTICIPANT_INDIV_REF = eNC_PARTICIPANT_INDIV_REF;
 	}
 
 	public String getENC_HOSP_ADMSRC_CODING_CODE() 
 	{
-		return ENC_HOSP_ADMSRC_CODING_CODE;
+		return this.ENC_HOSP_ADMSRC_CODING_CODE;
 	}
 
 	public void setENC_HOSP_ADMSRC_CODING_CODE(String eNC_HOSP_ADMSRC_CODING_CODE) 
 	{
-		ENC_HOSP_ADMSRC_CODING_CODE = eNC_HOSP_ADMSRC_CODING_CODE;
+		this.ENC_HOSP_ADMSRC_CODING_CODE = eNC_HOSP_ADMSRC_CODING_CODE;
 	}
 
 	public String getENC_HOSP_ADMSRC_TEXT() 
 	{
-		return ENC_HOSP_ADMSRC_TEXT;
+		return this.ENC_HOSP_ADMSRC_TEXT;
 	}
 
 	public void setENC_HOSP_ADMSRC_TEXT(String eNC_HOSP_ADMSRC_TEXT) 
 	{
-		ENC_HOSP_ADMSRC_TEXT = eNC_HOSP_ADMSRC_TEXT;
+		this.ENC_HOSP_ADMSRC_TEXT = eNC_HOSP_ADMSRC_TEXT;
 	}
 
 	public String getENC_DIAGN_CON_REF() 
 	{
-		return ENC_DIAGN_CON_REF;
+		return this.ENC_DIAGN_CON_REF;
 	}
 
 	public void setENC_DIAGN_CON_REF(String eNC_DIAGN_CON_REF) 
 	{
-		ENC_DIAGN_CON_REF = eNC_DIAGN_CON_REF;
+		this.ENC_DIAGN_CON_REF = eNC_DIAGN_CON_REF;
 	}
 
 	public String getENC_DIAGN_ROLE_CODING_CODE() 
 	{
-		return ENC_DIAGN_ROLE_CODING_CODE;
+		return this.ENC_DIAGN_ROLE_CODING_CODE;
 	}
 
 	public void setENC_DIAGN_ROLE_CODING_CODE(String eNC_DIAGN_ROLE_CODING_CODE) 
 	{
-		ENC_DIAGN_ROLE_CODING_CODE = eNC_DIAGN_ROLE_CODING_CODE;
+		this.ENC_DIAGN_ROLE_CODING_CODE = eNC_DIAGN_ROLE_CODING_CODE;
 	}
 
 	public String getENC_DIAGN_RANK() 
 	{
-		return ENC_DIAGN_RANK;
+		return this.ENC_DIAGN_RANK;
 	}
 
 	public void setENC_DIAGN_RANK(String eNC_DIAGN_RANK) 
 	{
-		ENC_DIAGN_RANK = eNC_DIAGN_RANK;
+		this.ENC_DIAGN_RANK = eNC_DIAGN_RANK;
 	}
 	
 	public String getENC_HOSP_DISDISP_CODING_SYST() 
 	{
-		return ENC_HOSP_DISDISP_CODING_SYST;
+		return this.ENC_HOSP_DISDISP_CODING_SYST;
 	}
 
 	public void setENC_HOSP_DISDISP_CODING_SYST(String eNC_HOSP_DISDISP_CODING_SYST) 
 	{
-		ENC_HOSP_DISDISP_CODING_SYST = eNC_HOSP_DISDISP_CODING_SYST;
+		this.ENC_HOSP_DISDISP_CODING_SYST = eNC_HOSP_DISDISP_CODING_SYST;
 	}
 	
 	public String getENC_DIAGN_ROLE_SYST() 
 	{
-		return ENC_DIAGN_ROLE_SYST;
+		return this.ENC_DIAGN_ROLE_SYST;
 	}
 
 	public void setENC_DIAGN_ROLE_SYST(String eNC_DIAGN_ROLE_SYST) 
 	{
-		ENC_DIAGN_ROLE_SYST = eNC_DIAGN_ROLE_SYST;
+		this.ENC_DIAGN_ROLE_SYST = eNC_DIAGN_ROLE_SYST;
 	}
 	
 	public String getENC_STATUS() 
 	{
-		return ENC_STATUS;
+		return this.ENC_STATUS;
 	}
 
 	public void setENC_STATUS(String eNC_STATUS)
 	{
-		ENC_STATUS = eNC_STATUS;
+		this.ENC_STATUS = eNC_STATUS;
 	}
 
 	@Override
