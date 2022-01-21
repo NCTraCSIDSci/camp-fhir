a7ec6b7c4fbb540385a4d9de7aa3d8968551e213
jameschump <champioj@email.unc.edu>
2022-01-21 10:53:47 -0500

    Update server.js

--- GUIServer/server.js
+++ GUIServer/server.js
@@ -1,32 +1,32 @@
-var http = require('http');  
+/*
+var http = require('http');
 var fs = require('fs');
 
 const express = require('express')
 const app = express()
 
-//read the server values and write the files to a file
 app.use(
     express.urlencoded({
       extended: true
     })
   )
-  
+
   app.use(express.json())
-  
+
   app.post('/8000', (req, res) => {
     console.log(req.body)
     fs.writeFile('text.txt', req.body, err => {
         if (err) {
          console.error(err)
          return
-        } 
+        }
     })
   })
 
 function onRequest (request, response) {
-    response.writeHead(200, {  
-        'Content-Type': 'text/html'  
-    });  
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
     fs.readFile('./GUIServer/server.html', null, function(error, data){
         console.log("this server is running");
         if(error) {
@@ -35,11 +35,33 @@ function onRequest (request, response) {
         } else {
             //write the HTML file or render it
             response.write(data);
-            
+
         }
         response.end();
     })
 }
 http.createServer(onRequest).listen(8000);
+*/
 
+const express = require('express');
+var bodyParser = require('body-parser')
+const app = express();
+var fs = require('fs');
 
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', urlencodedParser, (req, res) => {
+    res.sendStatus(200);
+    fs.writeFile('config.json', JSON.stringify(req.body), err => {
+        if (err) {
+         console.error(err)
+         return
+        }
+    })
+});
+
+app.listen(3000);
