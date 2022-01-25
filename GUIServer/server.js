bb8f339e886864ac126a9cee1ab61ab2d4cb71a4
asiyahmad <90564381+asiyahmad@users.noreply.github.com>
2022-01-25 13:31:10 -0500

    add scrape of HTML table

--- GUIServer/server.js
+++ GUIServer/server.js
@@ -1,48 +1,3 @@
-/*
-var http = require('http');
-var fs = require('fs');
-
-const express = require('express')
-const app = express()
-
-app.use(
-    express.urlencoded({
-      extended: true
-    })
-  )
-
-  app.use(express.json())
-
-  app.post('/8000', (req, res) => {
-    console.log(req.body)
-    fs.writeFile('text.txt', req.body, err => {
-        if (err) {
-         console.error(err)
-         return
-        }
-    })
-  })
-
-function onRequest (request, response) {
-    response.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
-    fs.readFile('./GUIServer/server.html', null, function(error, data){
-        console.log("this server is running");
-        if(error) {
-            response.writeHead(404);
-            response.write('File got lost');
-        } else {
-            //write the HTML file or render it
-            response.write(data);
-
-        }
-        response.end();
-    })
-}
-http.createServer(onRequest).listen(8000);
-*/
-
 const express = require('express');
 var bodyParser = require('body-parser')
 const app = express();
@@ -56,6 +11,7 @@ app.get('/', (req, res) => {
 
 app.post('/', urlencodedParser, (req, res) => {
     res.sendStatus(200);
+   
     fs.writeFile('config.json', JSON.stringify(req.body), err => {
         if (err) {
          console.error(err)
