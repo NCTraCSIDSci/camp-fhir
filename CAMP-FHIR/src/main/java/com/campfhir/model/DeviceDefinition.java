a9b69948e382220685f84ffa7ab05ffa7c803320
jameschump <champioj@email.unc.edu>
2022-02-23 13:24:23 -0500

    Add files via upload

---
+++ CAMP-FHIR/src/main/java/com/campfhir/model/DeviceDefinition.java
@@ -0,0 +1,279 @@
+package main.java.com.campfhir.model;
+
+
+public class  DeviceDefinition implements java.io.Serializable 
+{
+	 public String getDevdef_id() {
+		return devdef_id;
+	}
+	public void setDevdef_id(String devdef_id) {
+		this.devdef_id = devdef_id;
+	}
+	public String getDevdef_udideviceidentifier() {
+		return devdef_udideviceidentifier;
+	}
+	public void setDevdef_udideviceidentifier(String devdef_udideviceidentifier) {
+		this.devdef_udideviceidentifier = devdef_udideviceidentifier;
+	}
+	public String getDevdef_udiid_deviceidentifier() {
+		return devdef_udiid_deviceidentifier;
+	}
+	public void setDevdef_udiid_deviceidentifier(String devdef_udiid_deviceidentifier) {
+		this.devdef_udiid_deviceidentifier = devdef_udiid_deviceidentifier;
+	}
+	public String getDevdef_udiid_issuer() {
+		return devdef_udiid_issuer;
+	}
+	public void setDevdef_udiid_issuer(String devdef_udiid_issuer) {
+		this.devdef_udiid_issuer = devdef_udiid_issuer;
+	}
+	public String getDevdef_udiid_jurisdiction() {
+		return devdef_udiid_jurisdiction;
+	}
+	public void setDevdef_udiid_jurisdiction(String devdef_udiid_jurisdiction) {
+		this.devdef_udiid_jurisdiction = devdef_udiid_jurisdiction;
+	}
+	public String getDevdef_manufacturer_x() {
+		return devdef_manufacturer_x;
+	}
+	public void setDevdef_manufacturer_x(String devdef_manufacturer_x) {
+		this.devdef_manufacturer_x = devdef_manufacturer_x;
+	}
+	public String getDevdef_manufacturerString() {
+		return devdef_manufacturerString;
+	}
+	public void setDevdef_manufacturerString(String devdef_manufacturerString) {
+		this.devdef_manufacturerString = devdef_manufacturerString;
+	}
+	public String getDevdef_manufacturerreference() {
+		return devdef_manufacturerreference;
+	}
+	public void setDevdef_manufacturerreference(String devdef_manufacturerreference) {
+		this.devdef_manufacturerreference = devdef_manufacturerreference;
+	}
+	public String getDevdef_devicename() {
+		return devdef_devicename;
+	}
+	public void setDevdef_devicename(String devdef_devicename) {
+		this.devdef_devicename = devdef_devicename;
+	}
+	public String getDevdef_devicename_name() {
+		return devdef_devicename_name;
+	}
+	public void setDevdef_devicename_name(String devdef_devicename_name) {
+		this.devdef_devicename_name = devdef_devicename_name;
+	}
+	public String getDevdef_devicename_type() {
+		return devdef_devicename_type;
+	}
+	public void setDevdef_devicename_type(String devdef_devicename_type) {
+		this.devdef_devicename_type = devdef_devicename_type;
+	}
+	public String getDevdef_modelnumber() {
+		return devdef_modelnumber;
+	}
+	public void setDevdef_modelnumber(String devdef_modelnumber) {
+		this.devdef_modelnumber = devdef_modelnumber;
+	}
+	public String getDevdef_type() {
+		return devdef_type;
+	}
+	public void setDevdef_type(String devdef_type) {
+		this.devdef_type = devdef_type;
+	}
+	public String getDevdef_specialization() {
+		return devdef_specialization;
+	}
+	public void setDevdef_specialization(String devdef_specialization) {
+		this.devdef_specialization = devdef_specialization;
+	}
+	public String getDevdef_specialization_systemtp() {
+		return devdef_specialization_systemtp;
+	}
+	public void setDevdef_specialization_systemtp(String devdef_specialization_systemtp) {
+		this.devdef_specialization_systemtp = devdef_specialization_systemtp;
+	}
+	public String getDevdef_specialization_version() {
+		return devdef_specialization_version;
+	}
+	public void setDevdef_specialization_version(String devdef_specialization_version) {
+		this.devdef_specialization_version = devdef_specialization_version;
+	}
+	public String getDevdef_version() {
+		return devdef_version;
+	}
+	public void setDevdef_version(String devdef_version) {
+		this.devdef_version = devdef_version;
+	}
+	public String getDevdef_safety() {
+		return devdef_safety;
+	}
+	public void setDevdef_safety(String devdef_safety) {
+		this.devdef_safety = devdef_safety;
+	}
+	public String getDevdef_shelflifestorage() {
+		return devdef_shelflifestorage;
+	}
+	public void setDevdef_shelflifestorage(String devdef_shelflifestorage) {
+		this.devdef_shelflifestorage = devdef_shelflifestorage;
+	}
+	public String getDevdef_physicalcharacteristics() {
+		return devdef_physicalcharacteristics;
+	}
+	public void setDevdef_physicalcharacteristics(String devdef_physicalcharacteristics) {
+		this.devdef_physicalcharacteristics = devdef_physicalcharacteristics;
+	}
+	public String getDevdef_languagecode() {
+		return devdef_languagecode;
+	}
+	public void setDevdef_languagecode(String devdef_languagecode) {
+		this.devdef_languagecode = devdef_languagecode;
+	}
+	public String getDevdef_capability() {
+		return devdef_capability;
+	}
+	public void setDevdef_capability(String devdef_capability) {
+		this.devdef_capability = devdef_capability;
+	}
+	public String getDevdef_capability_type() {
+		return devdef_capability_type;
+	}
+	public void setDevdef_capability_type(String devdef_capability_type) {
+		this.devdef_capability_type = devdef_capability_type;
+	}
+	public String getDevdef_capability_description() {
+		return devdef_capability_description;
+	}
+	public void setDevdef_capability_description(String devdef_capability_description) {
+		this.devdef_capability_description = devdef_capability_description;
+	}
+	public String getDevdef_property() {
+		return devdef_property;
+	}
+	public void setDevdef_property(String devdef_property) {
+		this.devdef_property = devdef_property;
+	}
+	public String getDevdef_property_type() {
+		return devdef_property_type;
+	}
+	public void setDevdef_property_type(String devdef_property_type) {
+		this.devdef_property_type = devdef_property_type;
+	}
+	public String getDevdef_property_valuequantity() {
+		return devdef_property_valuequantity;
+	}
+	public void setDevdef_property_valuequantity(String devdef_property_valuequantity) {
+		this.devdef_property_valuequantity = devdef_property_valuequantity;
+	}
+	public String getDevdef_property_valuecode() {
+		return devdef_property_valuecode;
+	}
+	public void setDevdef_property_valuecode(String devdef_property_valuecode) {
+		this.devdef_property_valuecode = devdef_property_valuecode;
+	}
+	public String getDevdef_owner_ref() {
+		return devdef_owner_ref;
+	}
+	public void setDevdef_owner_ref(String devdef_owner_ref) {
+		this.devdef_owner_ref = devdef_owner_ref;
+	}
+	public String getDevdef_contact() {
+		return devdef_contact;
+	}
+	public void setDevdef_contact(String devdef_contact) {
+		this.devdef_contact = devdef_contact;
+	}
+	public String getDevdef_url() {
+		return devdef_url;
+	}
+	public void setDevdef_url(String devdef_url) {
+		this.devdef_url = devdef_url;
+	}
+	public String getDevdef_onlineinformation() {
+		return devdef_onlineinformation;
+	}
+	public void setDevdef_onlineinformation(String devdef_onlineinformation) {
+		this.devdef_onlineinformation = devdef_onlineinformation;
+	}
+	public String getDevdef_note() {
+		return devdef_note;
+	}
+	public void setDevdef_note(String devdef_note) {
+		this.devdef_note = devdef_note;
+	}
+	public String getDevdef_quantity() {
+		return devdef_quantity;
+	}
+	public void setDevdef_quantity(String devdef_quantity) {
+		this.devdef_quantity = devdef_quantity;
+	}
+	public String getDevdef_parentdevice() {
+		return devdef_parentdevice;
+	}
+	public void setDevdef_parentdevice(String devdef_parentdevice) {
+		this.devdef_parentdevice = devdef_parentdevice;
+	}
+	public String getDevdef_material() {
+		return devdef_material;
+	}
+	public void setDevdef_material(String devdef_material) {
+		this.devdef_material = devdef_material;
+	}
+	public String getDevdef_material_substance() {
+		return devdef_material_substance;
+	}
+	public void setDevdef_material_substance(String devdef_material_substance) {
+		this.devdef_material_substance = devdef_material_substance;
+	}
+	public String getDevdef_material_alternate() {
+		return devdef_material_alternate;
+	}
+	public void setDevdef_material_alternate(String devdef_material_alternate) {
+		this.devdef_material_alternate = devdef_material_alternate;
+	}
+	public String getDevdef_material_allerginindic() {
+		return devdef_material_allerginindic;
+	}
+	public void setDevdef_material_allerginindic(String devdef_material_allerginindic) {
+		this.devdef_material_allerginindic = devdef_material_allerginindic;
+	}
+	private String devdef_id;
+	 private String devdef_udideviceidentifier;
+	 private String devdef_udiid_deviceidentifier;
+	 private String devdef_udiid_issuer;
+	 private String devdef_udiid_jurisdiction;
+	 private String devdef_manufacturer_x;
+	 private String devdef_manufacturerString;
+	 private String devdef_manufacturerreference;
+	 private String devdef_devicename;
+	 private String devdef_devicename_name;
+	 private String devdef_devicename_type;
+	 private String devdef_modelnumber;
+	 private String devdef_type;
+	 private String devdef_specialization;
+	 private String devdef_specialization_systemtp;
+	 private String devdef_specialization_version;
+	 private String devdef_version;
+	 private String devdef_safety;
+	 private String devdef_shelflifestorage;
+	 private String devdef_physicalcharacteristics;
+	 private String devdef_languagecode;
+	 private String devdef_capability;
+	 private String devdef_capability_type;
+	 private String devdef_capability_description;
+	 private String devdef_property;
+	 private String devdef_property_type;
+	 private String devdef_property_valuequantity;
+	 private String devdef_property_valuecode;
+	 private String devdef_owner_ref;
+	 private String devdef_contact;
+	 private String devdef_url;
+	 private String devdef_onlineinformation;
+	 private String devdef_note;
+	 private String devdef_quantity;
+	 private String devdef_parentdevice;
+	 private String devdef_material;
+	 private String devdef_material_substance;
+	 private String devdef_material_alternate;
+	 private String devdef_material_allerginindic;
+}
