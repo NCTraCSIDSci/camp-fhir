154f9c3ddf8ad1bd16acdd75b02daef082d8faa4
jameschump <champioj@email.unc.edu>
2022-03-01 19:09:55 -0500

    Add files via upload

--- CAMP-FHIR/src/main/java/com/campfhir/service/ConditionService.java
+++ CAMP-FHIR/src/main/java/com/campfhir/service/ConditionService.java
@@ -1,8 +1,10 @@
-package com.campfhir.service;
+package main.java.com.campfhir.service;
 
 import java.io.BufferedWriter;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,15 +13,15 @@ import javax.xml.parsers.ParserConfigurationException;
 
 import org.hibernate.ScrollableResults;
 import org.hibernate.Session;
-import org.hl7.fhir.dstu3.model.Bundle;
-import org.hl7.fhir.dstu3.model.Bundle.BundleType;
+import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.Bundle.BundleType;
 import org.hl7.fhir.exceptions.FHIRException;
 import org.xml.sax.SAXException;
 
-import com.campfhir.dao.ConditionDao;
-import com.campfhir.model.Condition;
-import com.campfhir.model.Encounter;
-import com.campfhir.service.conversion.ConditionConversion;
+import main.java.com.campfhir.dao.ConditionDao;
+import main.java.com.campfhir.model.Campfhir;
+import main.java.com.campfhir.model.Condition;
+import main.java.com.campfhir.service.conversion.ConditionConversion;
 
 import ca.uhn.fhir.context.FhirContext;
 
@@ -27,7 +29,7 @@ import ca.uhn.fhir.context.FhirContext;
 *
 * @author  James Champion
 * @version 1.0
-* @since   2019-08-20 
+* @since   2019-02-08 
 */
 public class ConditionService 
 {
@@ -40,7 +42,7 @@ public class ConditionService
 		conditionDao = new ConditionDao();
 		cc = new ConditionConversion();
 	}
-
+/*
 	public void persist(Condition entity) throws ParserConfigurationException, SAXException, IOException 
 	{
 		conditionDao.openCurrentSessionwithTransaction();
@@ -78,24 +80,24 @@ public class ConditionService
 		conditionDao.delete(condition);
 		conditionDao.closeCurrentSessionwithTransaction();
 	}
-
-	public void findAll(int partition, String path) throws ParserConfigurationException, SAXException, IOException, ParseException 
+*/
+	public void findAll(int partition, Campfhir cf) throws ParserConfigurationException, SAXException, IOException, ParseException 
 	{
-		Session session = conditionDao.openCurrentSession();
+		Session session = conditionDao.openCurrentSession(cf);
 		ScrollableResults conditions = conditionDao.findAll();
 		
-	     int i = 0;
+	     int i = 1;
 	     System.out.println("start");
 	     Bundle bundle = new Bundle().setType(BundleType.COLLECTION);
 	     
-
 	     while ( conditions.next() ) 
 	     {	
 			if ((i % partition) == 0)
 			{
-		    	session.flush();
-		    	session.clear();
-		    	writeFile(path, i, bundle);
+				System.out.println(i);
+				session.clear();
+				
+		    	writeFile(cf.getOutputfolder(), i, bundle);
 			    bundle = new Bundle().setType(BundleType.COLLECTION);
 			}
 			
@@ -105,11 +107,9 @@ public class ConditionService
 			   .setFullUrl("https://www.hl7.org/fhir/condition.html")  			   
 			   .setResource(cc.Conditions((Condition) conditions.get(0)));
 			
-			
-			System.out.println(i);
 	     }
 	     
-	     writeFile(path, i, bundle);
+	     writeFile(cf.getOutputfolder(), i, bundle);
 		 conditionDao.closeCurrentSession();
 	}
 	
@@ -126,7 +126,7 @@ public class ConditionService
 		return e;
 	}
 
-	
+	/*
 	
 	public void deleteAll() throws ParserConfigurationException, SAXException, IOException 
 	{
@@ -134,7 +134,7 @@ public class ConditionService
 		conditionDao.deleteAll();
 		conditionDao.closeCurrentSessionwithTransaction();
 	}
-
+*/
 	public ConditionDao conditionDao() 
 	{
 		return conditionDao;
@@ -146,11 +146,9 @@ public class ConditionService
 		
 		try 
 		{
-			BufferedWriter writer;
+			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(path+"/"+domain+".json", true), StandardCharsets.UTF_8));
 			
-			writer = new BufferedWriter(new FileWriter(path+"/"+domain+".json"));
-			
-			String file = FhirContext.forDstu3().newJsonParser().setPrettyPrint(true).encodeResourceToString(bundle);
+			String file = FhirContext.forR4().newJsonParser().setPrettyPrint(false).encodeResourceToString(bundle);			
 			
 		    writer.write(file);
 		    writer.close();
