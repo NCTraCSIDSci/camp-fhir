a5d5282004cf73edef6551748aea76d4d6e03e3b
james <james@unc-mac-james.lan>
2022-09-01 17:20:59 -0400

    New Dao Commit!

---
+++ CAMP-FHIR/src/main/java/com/campfhir/dao/ResourceDao.java
@@ -0,0 +1,123 @@
+package main.java.com.campfhir.dao;
+
+import java.io.IOException;
+
+
+import javax.xml.parsers.ParserConfigurationException;
+
+
+import org.hibernate.ScrollMode;
+import org.hibernate.ScrollableResults;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hl7.fhir.r4.model.Resource;
+import org.xml.sax.SAXException;
+
+import main.java.com.campfhir.model.Campfhir;
+
+import main.java.utils.HibernateBaseDB;
+
+/**
+*
+* @author  James Champion
+* @version 1.0
+* @since   2019-02-08 
+*/
+public class ResourceDao implements ResourceDaoInterface<Resource, String> 
+{
+	private Session currentSession;
+	private SessionFactory sessionFactory;
+	
+	private Transaction currentTransaction;
+
+	public ResourceDao() {}
+
+	public Session openCurrentSession(Campfhir cf) 
+			throws ParserConfigurationException, SAXException, IOException 
+	{
+		sessionFactory = HibernateBaseDB.getSessionFactory(cf);
+		currentSession = sessionFactory.openSession();
+		return currentSession;
+	}
+
+//	public Session openCurrentSessionwithTransaction() 
+//			throws ParserConfigurationException, SAXException, IOException 
+//	{
+//		sessionFactory = HibernateBaseDB.getSessionFactory();
+//		currentSession = sessionFactory.openSession();
+//		currentTransaction = currentSession.beginTransaction();
+//		return currentSession;
+//	}
+	
+	public void closeCurrentSession() 
+	{
+		currentSession.close();
+	}
+	
+	public void closeCurrentSessionwithTransaction() 
+	{
+		currentTransaction.commit();
+		currentSession.close();
+	}
+	
+	private static SessionFactory getSessionFactory() 
+	{
+		Configuration configuration = new Configuration().configure();
+		StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+				.applySettings(configuration.getProperties());
+		SessionFactory sessionFactory = configuration.buildSessionFactory(builder.build());
+		return sessionFactory;
+	}
+
+	public Session getCurrentSession() 
+	{
+		return currentSession;
+	}
+
+	public void setCurrentSession(Session currentSession) 
+	{
+		this.currentSession = currentSession;
+	}
+
+	public Transaction getCurrentTransaction() 
+	{
+		return currentTransaction;
+	}
+
+	public void setCurrentTransaction(Transaction currentTransaction) 
+	{
+		this.currentTransaction = currentTransaction;
+	}
+
+	public void persist(Resource entity) 
+	{
+		getCurrentSession().save(entity);
+	}
+
+//	public ScrollableResults findAll(int start, int max) 
+//	{
+//		ScrollableResults patients = getCurrentSession().createQuery("FROM Patient order by ID ASC")
+//				.setFirstResult(start)
+//				.setMaxResults(max)
+//				.setReadOnly(true)
+//		        .setCacheable(true)
+//		    .scroll(ScrollMode.FORWARD_ONLY);
+//		
+//
+//
+//		return patients;
+//	}
+	
+	public ScrollableResults findAll(Campfhir cf) 
+	{
+		ScrollableResults patients = getCurrentSession().createQuery("FROM "+cf.getResource())
+				.setReadOnly(true)
+		        .setCacheable(true)
+		    .scroll(ScrollMode.FORWARD_ONLY);
+		
+		return patients;
+	}
+}
