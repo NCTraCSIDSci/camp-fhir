edbe44c5c56b0ae8ec602af52b7a61dc10276962
pikovach <pikovach@live.unc.edu>
2022-11-18 11:46:32 -0500

    Adding FHIR path to SQL and to XML

--- CAMP-FHIR/src/main/resources/SQL/ResourceSQL/Linkage.sql
+++ CAMP-FHIR/src/main/resources/SQL/ResourceSQL/Linkage.sql
@@ -1,10 +1,10 @@
 drop table if exists `Linkage`;
 CREATE TABLE `Linkage` (
  id varchar(64),
-Linkage_Active TEXT,
-Linkage_Athr TEXT,
-Linkage_Itm_Rsrc TEXT,
-Linkage_Itm_Typ TEXT,
+Linkage_Active TEXT COMMENT 'Linkage.Active',
+Linkage_Athr TEXT COMMENT 'Linkage.Author',
+Linkage_Itm_Rsrc TEXT COMMENT 'Linkage.Item.Resource',
+Linkage_Itm_Typ TEXT COMMENT 'Linkage.Item.Type',
 PRIMARY KEY(id)) ENGINE=MyISAM
     ROW_FORMAT=COMPRESSED 
     KEY_BLOCK_SIZE=8;
--- CAMP-FHIR/src/main/resources/SQL/ResourceSQL/OperationOutcome.sql
+++ CAMP-FHIR/src/main/resources/SQL/ResourceSQL/OperationOutcome.sql
@@ -1,17 +1,17 @@
 drop table if exists `OperationOutcome`;
 CREATE TABLE `OperationOutcome` (
  id varchar(64),
-OprtnOutcome_Issue_Cd TEXT,
-OprtnOutcome_Issue_Dtls_Cdg_Cd TEXT,
-OprtnOutcome_Issue_Dtls_Cdg_Dsply TEXT,
-OprtnOutcome_Issue_Dtls_Cdg_Sys TEXT,
-OprtnOutcome_Issue_Dtls_Cdg_UsrSltd TEXT,
-OprtnOutcome_Issue_Dtls_Cdg_Vrsn TEXT,
-OprtnOutcome_Issue_Dtls_Txt TEXT,
-OprtnOutcome_Issue_Diagnostics TEXT,
-OprtnOutcome_Issue_Exprsn TEXT,
-OprtnOutcome_Issue_Lctn TEXT,
-OprtnOutcome_Issue_Severity TEXT,
+OprtnOutcome_Issue_Cd TEXT COMMENT 'OperationOutcome.Issue.Code',
+OprtnOutcome_Issue_Dtls_Cdg_Cd TEXT COMMENT 'OperationOutcome.Issue.Details.Coding.Code',
+OprtnOutcome_Issue_Dtls_Cdg_Dsply TEXT COMMENT 'OperationOutcome.Issue.Details.Coding.Display',
+OprtnOutcome_Issue_Dtls_Cdg_Sys TEXT COMMENT 'OperationOutcome.Issue.Details.Coding.System',
+OprtnOutcome_Issue_Dtls_Cdg_UsrSltd TEXT COMMENT 'OperationOutcome.Issue.Details.Coding.UserSelected',
+OprtnOutcome_Issue_Dtls_Cdg_Vrsn TEXT COMMENT 'OperationOutcome.Issue.Details.Coding.Version',
+OprtnOutcome_Issue_Dtls_Txt TEXT COMMENT 'OperationOutcome.Issue.Details.Text',
+OprtnOutcome_Issue_Diagnostics TEXT COMMENT 'OperationOutcome.Issue.Diagnostics',
+OprtnOutcome_Issue_Exprsn TEXT COMMENT 'OperationOutcome.Issue.Expression',
+OprtnOutcome_Issue_Lctn TEXT COMMENT 'OperationOutcome.Issue.Location',
+OprtnOutcome_Issue_Severity TEXT COMMENT 'OperationOutcome.Issue.Severity',
 PRIMARY KEY(id)) ENGINE=MyISAM
     ROW_FORMAT=COMPRESSED 
     KEY_BLOCK_SIZE=8;
