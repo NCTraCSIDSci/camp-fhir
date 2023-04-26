dfa95229349902c26353e6ae90e3ae447c94c5e9
jameschump <champioj@email.unc.edu>
2023-04-26 12:24:48 -0400

    Update HibernateBaseDB.java

--- CAMP-FHIR/src/main/java/utils/HibernateBaseDB.java
+++ CAMP-FHIR/src/main/java/utils/HibernateBaseDB.java
@@ -1,6 +1,5 @@
 package main.java.utils;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Properties;
@@ -23,7 +22,7 @@ import main.java.com.campfhir.model.Campfhir;
 *
 * @author  James Champion
 * @version 1.0
-* @since   2019-02-08 
+* @since   2023-04-18
 */
 public class HibernateBaseDB 
 {
@@ -37,8 +36,7 @@ public class HibernateBaseDB
     public static SessionFactory getSessionFactory(Campfhir cf, String direction) 
     		throws ParserConfigurationException, SAXException, IOException 
     {
-        Configuration configuration = new Configuration();
-        
+        Configuration configuration = new Configuration();        
         if(direction.equals("in"))
         {
 	        dialect = cf.getiSqldialects();
@@ -47,6 +45,7 @@ public class HibernateBaseDB
 	        username = cf.getiDbusername();
 	        password = cf.getiDbpassword();
         }
+        
         else if(direction.equals("out"))
         {
 	        dialect = cf.getoSqldialects();
@@ -55,24 +54,20 @@ public class HibernateBaseDB
 	        username = cf.getoDbusername();
 	        password = cf.getoDbpassword();
         }
-        
-        
-        
+
         Properties properties = new Properties();
         properties.put("hibernate.dialect", dialect);
-        properties.put("hibernate.connection.driver_class", driver_class);
+        properties.put("hibernate.connection.driver_class",driverClass().get(driver_class));
         properties.put("hibernate.connection.url", url);
         properties.put("hibernate.connection.username", username);
         properties.put("hibernate.connection.password", password);
         properties.put("hibernate.current_session_context_class", "thread");
         properties.put("show_sql", "true");
+        properties.put("hibernate.default_catalog", "true");
 
         configuration.setProperties(properties);
-        configuration.addFile(new File("./").getCanonicalPath()+"/"+cf.getResource()+".hbm.xml"); // C:\Users\pikovach\camp-fhir\CAMP-FHIR\Patient.hbm.xml
-
-        // configuration.addResource(cf.getResource()+".hbm.xml");
-//        configuration.addResource("C:\\Users\\pikovach\\camp-fhir\\CAMP-FHIR\\Patient.hbm.xml");
-        
+        configuration.addResource(cf.getResource()+".hbm.xml");
+               
         ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
         .applySettings(configuration.getProperties()).build();
 
@@ -81,36 +76,38 @@ public class HibernateBaseDB
         return sessionFactory;
     }
     
-    public static HashMap<String, String> driverClass(String Dialect)
+    public static HashMap<String, String> driverClass()
     {
-    	   HashMap<String, String> classfiles = new HashMap<String, String>();
+    	   HashMap<String, String> dc = new HashMap<String, String>();
+    	   
+    	   dc.put("org.hibernate.dialect.H2Dialect", "org.h2.Driver");
+    	   dc.put("org.hibernate.dialect.PostgreSQLDialect", "org.postgresql.Driver");
+    	   dc.put("org.hibernate.dialect.OracleDialect", "oracle.jdbc.OracleDriver");
+    	   dc.put("org.hibernate.dialect.SQLServerDialect", "com.microsoft.sqlserver.jdbc.SQLServerDriver");
+    	   dc.put("org.xbib.jdbc.csv.CsvDriver", "oracle.jdbc.OracleDriver");
 
-    	    // Add keys and values (Country, City)
-    	    classfiles.put("org.hibernate.dialect.DB2Dialect", "");
-    	    classfiles.put("org.hibernate.dialect.DB2400Dialect", "");
-    	    classfiles.put("org.hibernate.dialect.DB2390Dialect", "");
-    	    classfiles.put("org.hibernate.dialect.PostgreSQLDialect", "");
-    	    classfiles.put("org.hibernate.dialect.MySQL5Dialect", "");
-    	    classfiles.put("org.hibernate.dialect.MySQL5InnoDBDialect", "");
-    	    classfiles.put("org.hibernate.dialect.MySQLMyISAMDialect", "");
-    	    classfiles.put("org.hibernate.dialect.OracleDialect", "");
-    	    classfiles.put("org.hibernate.dialect.Oracle9iDialect", "");
-    	    classfiles.put("org.hibernate.dialect.SybaseASE15Dialect", "");
-    	    classfiles.put("org.hibernate.dialect.SQLServerDialect", "");
-    	    classfiles.put("org.hibernate.dialect.SQLServer2008Dialect", "");
-    	    classfiles.put("org.hibernate.dialect.SAPDBDialect", "");
-    	    classfiles.put("org.hibernate.dialect.InformixDialect", "");
-    	    classfiles.put("org.hibernate.dialect.HSQLDialect", "");
-    	    classfiles.put("org.hibernate.dialect.H2Dialect", "org.h2.Driver");
-    	    classfiles.put("org.hibernate.dialect.IngresDialect", "");
-    	    classfiles.put("org.hibernate.dialect.ProgressDialect", "");
-    	    classfiles.put("org.hibernate.dialect.MckoiDialect", "");
-    	    classfiles.put("org.hibernate.dialect.InterbaseDialect", "");
-    	    classfiles.put("org.hibernate.dialect.PointbaseDialect", "");
-    	    classfiles.put("org.hibernate.dialect.FrontbaseDialect", "");
-    	    classfiles.put("org.hibernate.dialect.FirebirdDialect", "");
+    	   dc.put("org.hibernate.dialect.DB2Dialect", "");
+    	   dc.put("org.hibernate.dialect.DB2400Dialect", "");
+    	   dc.put("org.hibernate.dialect.DB2390Dialect", "");
+    	   dc.put("org.hibernate.dialect.MySQL5Dialect", "");
+    	   dc.put("org.hibernate.dialect.MySQL5InnoDBDialect", "");
+    	   dc.put("org.hibernate.dialect.MySQLMyISAMDialect", "");    	   
+    	   dc.put("org.hibernate.dialect.Oracle9iDialect", "");
+    	   dc.put("org.hibernate.dialect.SybaseASE15Dialect", "");
+    	   dc.put("org.hibernate.dialect.SQLServer2008Dialect", "");
+    	   dc.put("org.hibernate.dialect.SAPDBDialect", "");
+    	   dc.put("org.hibernate.dialect.InformixDialect", "");
+    	   dc.put("org.hibernate.dialect.HSQLDialect", ""); 
+    	   dc.put("org.hibernate.dialect.IngresDialect", "");
+    	   dc.put("org.hibernate.dialect.ProgressDialect", "");
+    	   dc.put("org.hibernate.dialect.MckoiDialect", "");
+    	   dc.put("org.hibernate.dialect.InterbaseDialect", "");
+    	   dc.put("org.hibernate.dialect.PointbaseDialect", "");
+    	   dc.put("org.hibernate.dialect.FrontbaseDialect", "");
+    	   dc.put("org.hibernate.dialect.FirebirdDialect", "");
     	    
-    	    return classfiles;
+    	   return dc;
 
     }
+
 }
